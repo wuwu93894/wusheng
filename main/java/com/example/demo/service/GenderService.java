1e1d3ed2522d5b1106544bb7883de74b5357ec3e
wuwu93894 <104983123+wuwu93894@users.noreply.github.com>
2022-05-06 17:17:57 +0900

    Delete main directory

--- main/java/com/example/demo/service/GenderService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import java.util.ArrayList;
-import com.example.demo.bean.Gender;
-
-public interface GenderService {
-	 public ArrayList<Gender> listGender();
-
-}
--- main/java/com/example/demo/service/NationalityService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import java.util.ArrayList;
-import com.example.demo.bean.Nationality;
-
-public interface NationalityService {
-	public ArrayList<Nationality> listNationality();
-}
